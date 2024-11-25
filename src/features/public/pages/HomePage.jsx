912c4ef6d0750f04ee024d24dabf2319bd54de02
blankverse123000 <blankverse123000@gmail.com>
2024-11-25 20:48:47 +0800

    feat/done_Home_Property_without_Responsive

--- src/features/public/pages/HomePage.jsx
+++ src/features/public/pages/HomePage.jsx
@@ -1,17 +1,19 @@
-
 import FeatureSection from "../components/FeatureSection";
 import HeroSection from "../components/HeroSection";
 import RealEstateAgency from "../components/RealEstateAgencySection/RealEstateAgency";
 import ServiceForYou from "../components/ServiceForYouSection/ServiceForYou";
 import HomeBlogGroupSection from "../components/HomeBlogGroupSection";
+import HomePopularProper from "../components/HomePopularProper";
+import HomePoperBenefit from "../components/HomePoperBenefit";
 const HomePage = () => {
   return (
     <section className="flex flex-col gap-20">
-        <RealEstateAgency />
-        <ServiceForYou/>
-        <HomeBlogGroupSection />
+      <RealEstateAgency />
+      <HomePopularProper />
+      <HomePoperBenefit />
+      <ServiceForYou />
+      <HomeBlogGroupSection />
     </section>
-
   );
 };
 
