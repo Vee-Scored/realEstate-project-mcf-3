ad614d1c89326ad7eb55cfbbc6421ebc8868a7bc
Hanhtunaung97 <littlehanhtunaung97>
2024-11-25 16:50:30 +0800

    feat: homePage-newLetters-compo

--- src/features/public/pages/HomePage.jsx
+++ src/features/public/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Container from "../../../components/Container";
+import NewLetters from "../../../components/NewLetters";
 import HomeBlogGroupSection from "../components/HomeBlogGroupSection";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <Container>
       <section className="flex flex-col gap-10 items-start">
         <HomeBlogGroupSection />
+        <NewLetters />
       </section>
     </Container>
   );
