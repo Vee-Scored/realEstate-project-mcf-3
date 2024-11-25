d365e0021525083d597ceb353c8f3ccfd443de69
Hanhtunaung97 <littlehanhtunaung97>
2024-11-25 17:47:19 +0800

    feat:newLetters compo container insert

--- src/features/public/pages/HomePage.jsx
+++ src/features/public/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import HeroSection from "../components/HeroSection";
 import RealEstateAgency from "../components/RealEstateAgencySection/RealEstateAgency";
 import ServiceForYou from "../components/ServiceForYouSection/ServiceForYou";
 import HomeBlogGroupSection from "../components/HomeBlogGroupSection";
+import NewLetters from "../../../components/NewLetters";
 const HomePage = () => {
   return (
     <section className="flex flex-col gap-20">
       <RealEstateAgency />
       <ServiceForYou />
       <HomeBlogGroupSection />
+      <NewLetters />
     </section>
   );
 };
