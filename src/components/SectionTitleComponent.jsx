51b802516cfff9a111296696523eebb4f82062d4
Vee-Scored <myomin9916@gmail.com>
2024-11-25 15:22:59 +0630

    refactor home blog

--- src/components/SectionTitleComponent.jsx
+++ src/components/SectionTitleComponent.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Line101 from "../assets/HomeAssets/Line101.png"
 const SectionTitleComponent = ({ name }) => {
   return (
-    <div className=" flex gap-2">
-      <div className=" bg-primary-500 w-1 h-full"></div>
+    <div className=" flex items-center gap-2">
+      <img src={Line101} alt="line" />
       <p className=" text-[16px]  leading-[19.36px]  text-primary-500">
         {name}
       </p>
