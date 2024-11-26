b2336e29525eb1d3b6532bf305c7699cf078715a
Hanhtunaung97 <littlehanhtunaung97>
2024-11-26 18:12:19 +0800

    feat:home-blog-section-add-slide

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -10,27 +10,29 @@ const BlogCard = ({ blog: { blog_image, title, content, date } }) => {
         className="h-64 w-full object-cover object-center rounded-lg"
       />
       <div className="py-8 px-5 flex flex-col items-start gap-4">
-        <p className="flex justify-start items-center gap-3">
+        <div className="flex justify-start items-center gap-3">
           <img src={userCircle} alt="user" className="size-5" />
           <span className="text-sm text-neutral-500">By admin</span>
           <div className="h-5 w-[1px] bg-neutral-500" />
           <span className="text-sm text-neutral-500">Estate</span>
-        </p>
+        </div>
         <h1 className="text-2xl font-semibold font-heading line-clamp-2">
           {title}
         </h1>
         <p className="text-sm text-neutral-500 font-normal line-clamp-3">
           {content}
         </p>
-        <div className="h-[1px] w-full bg-neutral-200" />
-        <div className="flex justify-between items-center w-full">
-          <div className="flex items-center gap-3">
-            <img src={calender} alt="calender" className="size-5" />
-            <p className="text-sm text-neutral-500">{date}</p>
+        <div className=" mt-auto flex flex-col gap-3 w-full items-start">
+          <div className="h-[1px] w-full bg-neutral-200" />
+          <div className="flex justify-between items-end w-full">
+            <div className="flex items-center gap-3">
+              <img src={calender} alt="calender" className="size-5" />
+              <p className="text-sm text-neutral-500">{date}</p>
+            </div>
+            <ViewAllBtn className={`bg-neutral-700 text-white`}>
+              View detail
+            </ViewAllBtn>
           </div>
-          <ViewAllBtn className={`bg-neutral-700 text-white`}>
-            View detail
-          </ViewAllBtn>
         </div>
       </div>
     </div>
