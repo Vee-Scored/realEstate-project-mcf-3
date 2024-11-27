47c57e526afd37cee744c1eb3b8b90d0e38aa67c
blankverse123000 <blankverse123000@gmail.com>
2024-11-27 16:00:17 +0800

    feat/added_Dream_Home_to_Proper_Page

--- src/features/public/pages/PropertyPage.jsx
+++ src/features/public/pages/PropertyPage.jsx
@@ -1,5 +1,6 @@
 import BreadCrumb from "../components/BreadCrumb";
 import AllPropertyCompo from "../components/propertySectionCompo/AllPropertyCompo";
+import DreamHome from "../components/propertySectionCompo/DreamHome";
 import ShowResult from "../components/propertySectionCompo/ShowResult";
 
 const PropertyPage = () => {
@@ -8,6 +9,7 @@ const PropertyPage = () => {
       <BreadCrumb currentPage={"Property"} />
       <AllPropertyCompo />
       <ShowResult />
+      <DreamHome />
     </div>
   );
 };
