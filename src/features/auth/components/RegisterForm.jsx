6bbd1bd616be527372997bda8fb44341e577e5d7
Hanhtunaung97 <littlehanhtunaung97>
2024-11-28 16:00:36 +0800

    feat: auth-register finish

--- src/features/auth/components/RegisterForm.jsx
+++ src/features/auth/components/RegisterForm.jsx
@@ -1,121 +1,206 @@
 import { Link } from "react-router-dom";
 import ButtonSpinner from "../../../components/ButtonSpinner";
 import useRegister from "../hooks/useRegister";
-
+import apple from "../../../assets/AuthAsset/apple.svg";
+import google from "../../../assets/AuthAsset/google.svg";
 const RegisterForm = () => {
-  const { register, handleSubmit, handleRegister, isSubmitting } =
-    useRegister();
+  const {
+    register,
+    handleSubmit,
+    handleRegister,
+    errors,
+    password,
+    isSubmitting,
+  } = useRegister();
 
   return (
-    <form
-      onSubmit={handleSubmit(handleRegister)}
-      className="space-y-4 md:space-y-6 bg-neutral-50"
-    >
-      <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          {...register("name")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="eg. John Doe"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your email
-        </label>
-        <input
-          type="email"
-          id="email"
-          {...register("email")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="eg. john@company.com"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          {...register("password")}
-          placeholder="••••••••"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="confirm-password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Confirm password
-        </label>
-        <input
-          type="password"
-          id="confirm-password"
-          placeholder="••••••••"
-          {...register("password_confirmation")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        />
-      </div>
-      <div className="flex items-start">
-        <div className="flex items-center h-5">
-          <input
-            id="terms"
-            aria-describedby="terms"
-            type="checkbox"
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-            required
-          />
+    <section className="flex flex-col gap-3 justify-center">
+      <div className="bg-neutral-50 p-6 border border-neutral-200 rounded">
+        <div className=" flex  flex-col gap-3  mb-5">
+          <div className="text-center">
+            <h2 className=" font-heading font-semibold text-neutral-700 text-2xl lg:text-4xl mb-2">
+              Create Account
+            </h2>
+            <p className="text-sm font-normal text-neutral-500">
+              Enter your information below to create your account
+            </p>
+          </div>
+
+          <div className="flex justify-between items-center w-full">
+            <button className="flex hover:bg-neutral-100 active:bg-neutral-200 duration-200 items-center gap-2 border border-neutral-200 rounded-md py-1.5 px-5">
+              <img src={apple} alt="apple" />
+              <span className="text-neutral-700 font-normal text-base">
+                {" "}
+                Apple
+              </span>
+            </button>
+            <button className="flex items-center hover:bg-neutral-100 active:bg-neutral-200 gap-2 border border-neutral-200 rounded-md py-1.5 px-5">
+              <img src={google} alt="google" />
+              <span className="text-neutral-700 font-normal text-base">
+                {" "}
+                Google
+              </span>
+            </button>
+          </div>
+          <div className=" flex justify-between items-center">
+            <div className="h-[1px] w-full bg-neutral-200" />
+            <span className=" font-normal text-neutral-500 text-xs px-1 py-2 bg-white text-center text-nowrap">
+              OR CONTINUE WITH
+            </span>
+            <div className="h-[1px] w-full bg-neutral-200" />
+          </div>
         </div>
-        <div className="ml-3 text-sm">
-          <label
-            htmlFor="terms"
-            className="font-light text-gray-500 dark:text-gray-300"
-          >
-            I accept the{" "}
-            <a
-              className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-              href="#"
+        <form
+          onSubmit={handleSubmit(handleRegister)}
+          className="space-y-2 lg:space-y-3"
+        >
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Terms and Conditions
-            </a>
-          </label>
-        </div>
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              disabled={isSubmitting}
+              {...register("name", {
+                required: true,
+                minLength: 3,
+                maxLength: 50,
+              })}
+              className={`${
+                errors.name
+                  ? "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                  : "focus:ring-green-300 focus:border-green-300 border-slate-300  "
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded disabled:opacity-75 focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              placeholder="eg. John Doe"
+            />
+            {errors.name?.type === "required" && (
+              <p className="text-red-500 mt-2 text-sm">Name is required !</p>
+            )}
+            {errors.name?.type === "minLength" && (
+              <p className="text-red-500 mt-2 text-sm">
+                Name must be at least 3 letters !
+              </p>
+            )}
+            {errors.name?.type === "maxLength" && (
+              <p className="text-red-500 mt-2 text-sm">
+                Name must not be at more than 50 letters !
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              email
+            </label>
+            <input
+              type="email"
+              id="email"
+              disabled={isSubmitting}
+              {...register("email", {
+                required: true,
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                },
+              })}
+              className={`${
+                errors.email
+                  ? "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                  : "focus:ring-green-300 focus:border-green-300 border-slate-300  "
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded disabled:opacity-75 focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              placeholder="eg. john@company.com"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register("password", {
+                required: true,
+                minLength: 8,
+              })}
+              disabled={isSubmitting}
+              placeholder="••••••••"
+              className={`${
+                errors.password
+                  ? "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                  : "focus:ring-green-300 focus:border-green-300 border-slate-300  "
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded disabled:opacity-75 focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              required
+            />
+            {errors.password?.type === "required" && (
+              <p className="text-red-500 mt-2 text-sm">
+                Password is required !
+              </p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p className="text-red-500 mt-2 text-sm">
+                Password must be at least 8 letters !
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="confirm-password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Confirm password
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              placeholder="••••••••"
+              disabled={isSubmitting}
+              {...register("password_confirmation", {
+                required: true,
+                validate: (value) =>
+                  value == password || "Passwords do not match",
+              })}
+              className={`${
+                errors.password_confirmation
+                  ? "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                  : "focus:ring-green-300 focus:border-green-300 border-slate-300  "
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded disabled:opacity-75 focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              required
+            />
+            {errors.password_confirmation?.type === "required" && (
+              <p className="text-red-500 mt-2 text-sm">
+                Please confirm your password !
+              </p>
+            )}
+            {errors.password_confirmation?.type === "validate" && (
+              <p className="text-red-500 mt-2 text-sm">
+                {errors.password_confirmation.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="w-full flex justify-center items-center gap-3 disabled:pointer-events-none disabled:opacity-80 text-white bg-neutral-700 hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-neutral-300 font-medium rounded  text-sm px-5 py-2.5 text-center dark:bg-neutral-600 dark:hover:bg-neutral-700 dark:focus:ring-neutral-800"
+          >
+            Create account
+            {isSubmitting && <ButtonSpinner />}
+          </button>
+        </form>
       </div>
-      <button
-        type="submit"
-        disabled={isSubmitting}
-        className="w-full flex justify-center items-center gap-3 disabled:pointer-events-none disabled:opacity-80 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Create an account
-        {isSubmitting && <ButtonSpinner />}
-      </button>
-      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-        Already have an account?{" "}
-        <Link
-          to="/login"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Login here
-        </Link>
+      <p className="text-xs font-normal text-neutral-500 text-center">
+        By clicking continue, you agree to our <br /> Terms of Service and
+        Privacy Policy.
       </p>
-    </form>
+    </section>
   );
 };
 
--- src/features/auth/pages/RegisterPage.jsx
+++ src/features/auth/pages/RegisterPage.jsx
@@ -1,13 +1,12 @@
 import { Toaster } from "react-hot-toast";
 import RegisterForm from "../components/RegisterForm";
-import { Link } from "react-router-dom";
 import Container from "../../../components/Container";
 import AuthFrame from "../components/AuthFrame";
 
 const RegisterPage = () => {
   return (
-    <section className={"bg-neutral-50"}>
-      <Container>
+    <section className={"bg-neutral-50 "}>
+      <Container className={"!px-0"}>
         <Toaster />
         <AuthFrame>
           <RegisterForm />
