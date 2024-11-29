5e73b8d11b63e8b7ad51dac3436e44ba405e1bb6
blankverse123000 <blankverse123000@gmail.com>
2024-11-30 02:59:39 +0800

    fix/fixed_similarCarsection_Responsive

--- src/features/public/components/propertySectionCompo/SimilarListCardSection.jsx
+++ src/features/public/components/propertySectionCompo/SimilarListCardSection.jsx
@@ -14,12 +14,12 @@ const SimilarListCardSection = () => {
   // console.log(resultList.slice(0, 3));
   return (
     <div>
-      <div className="xl:grid grid xl:grid-cols-3 gap-16 md:hidden grid-cols-1   justify-between items-center">
+      <div className="xl:grid grid xl:grid-cols-3 gap-6 md:hidden grid-cols-1   justify-between items-center ">
         {resultList.slice(0, 3).map((item) => (
           <PopularProperCard key={item.id} property={item} />
         ))}
       </div>
-      <div className="md:grid xl:hidden md:grid-cols-2 gap-16 hidden justify-between items-center">
+      <div className="md:grid xl:hidden md:grid-cols-2 gap-6 hidden justify-between items-center">
         {resultList.slice(0, 4).map((item, index) => (
           <PopularProperCard key={index} property={item} />
         ))}
